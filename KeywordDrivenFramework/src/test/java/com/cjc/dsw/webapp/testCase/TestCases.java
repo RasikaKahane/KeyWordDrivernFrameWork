1f56a9e915cecf2976feaab0e967ef8ca4ee5fea
Rasika <rasikashedage@yahoo.com>
2026-05-13 15:52:35 +0530

    2nd Commit

--- KeywordDrivenFramework/src/test/java/com/cjc/dsw/webapp/testCase/TestCases.java
+++ KeywordDrivenFramework/src/test/java/com/cjc/dsw/webapp/testCase/TestCases.java
@@ -1,19 +1,149 @@
 package com.cjc.dsw.webapp.testCase;
 
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.cjc.dsw.webapp.pageClass.AddBillingaddress;
+import com.cjc.dsw.webapp.pageClass.AddBook;
+import com.cjc.dsw.webapp.pageClass.AddPaymentmethod;
+import com.cjc.dsw.webapp.pageClass.AddShippingAddress;
+import com.cjc.dsw.webapp.pageClass.ConfirmOrderPage;
+import com.cjc.dsw.webapp.pageClass.RegisterLoginPage;
+import com.cjc.dsw.webapp.pageClass.ShoppingCart;
+import com.cjc.dsw.webapp.pageClass.SucessfullyConfirmOrderPage;
+import com.cjc.dsw.webapp.utility.comman;
+import com.cjc.dws.webapp.pages.AddPaymentInformation;
+import com.cjc.dws.webapp.utility.Common;
 
 public class TestCases {
-	
+	Logger log=Logger.getLogger(TestCases.class.getName());
+	WebDriver driver;
 	@BeforeSuite
 	public void openBrowser()
 	{
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Rasika\\eclipse-workspace1\\KeywordDrivenFramework\\src\\test\\resources\\Browser\\chromedriver.exe");
+		 driver=new ChromeDriver();
+		 log.info("Open Browser");
+	}
+	@BeforeTest
+	public void enterURL()
+	{
+		driver.get("https://demowebshop.tricentis.com/register");
+		log.info("Enter URL");
+	}
+	@BeforeClass
+	public void maximize()
+	{
+		log.info("Maximize Window");
+	}
+	@BeforeMethod
+	public void DBConnection()
+	{
+		log.info("Connect Database");
+	}
+    @Test (priority=1)
+    public void Registration(String fn, String ln, String email, String pw, String cpw)
+	{
+    	log.info("Registration Start");
+    	RegisterLoginPage RegLogPg= PageFactory.initElements(driver, RegisterLoginPage.class);
+    	RegLogPg.Register(ln, email, pw, cpw, cpw);
+    	log.warn("successfully done");
+	}
+    @Test(priority=2)
+	public void Book()
+	{
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		log.info("Add Books Product");
+		AddBook book = PageFactory.initElements(driver, AddBook.class);
+		book.addBook();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		
+	}
+	@Test(priority=3)
+	public void ShoppingCartP() throws InterruptedException
+	{
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		log.info("ShoppingCart");
+
+		ShoppingCart shopCart = PageFactory.initElements(driver, ShoppingCart.class);
+		shopCart.AddShoppingCart();
+	}
+	@Test(priority=4)
+	public void Billingaddress() throws IOException
+	{
+		AddBillingaddress billAdd = PageFactory.initElements(driver, AddBillingaddress.class);
+		
+		String Country =comman.getBrowser().getProperty("country");
+		String City = comman.getBrowser().getProperty("city");
+		String Address = comman.getBrowser().getProperty("address");
+		String PinCode = comman.getBrowser().getProperty("pincode");
+		String PhNo = comman.getBrowser().getProperty("phNo");
+		billAdd.BillingAddress(Country,City,Address,PinCode,PhNo);
+		log.info("Billingaddress");
 		
 	}
 	
-	public void enterURL()
+	@Test(priority=5)
+	public void Shippingaddress()
+	{
+		AddShippingAddress shipAdd = PageFactory.initElements(driver, AddShippingAddress.class);
+		shipAdd.ShippingAddress();
+		log.info("Shippingaddress");
+		
+	}
+	@Test(priority=6)
+	public void Paymentmethod()
 	{
 		
+		AddPaymentmethod PayMod = PageFactory.initElements(driver, AddPaymentmethod.class);
+	    PayMod.paymentMethod();
+	    log.info("Paymentmethod");
+		
+	}
+	@Test(priority=7)
+	public void Paymentinformation()
+	{
+	
+		
+		/*SoftAssert sa=new SoftAssert();
+		
+		String Ptext = driver.findElement(By.xpath("//table/tbody/tr/td/p")).getText();
+		sa.assertAll();
+		Assert.assertEquals("You will pay by COD", Ptext);
+		System.out.println(Ptext);*/
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		AddPaymentInformation PayInfo = PageFactory.initElements(driver, AddPaymentInformation.class);
+		PayInfo.PaymentInfo();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		log.info("Paymentinformation");
+	}
+	@Test(priority=8)
+	public void Confirmorder()
+	{
+		ConfirmOrderPage ConOrd = PageFactory.initElements(driver, ConfirmOrderPage.class);
+		ConOrd.ConfirmOrder();
+		log.info("Confirm Order");
+	}
+	@Test(priority=9)
+	public void SucessfullyConfirmOrder()
+	{
+	
+		
+	 log.info("Order Confirmation");
+	 SucessfullyConfirmOrderPage sucOrder=PageFactory.initElements(driver, SucessfullyConfirmOrderPage.class);
+	 
+	 log.info(sucOrder);
 	}
 
 }
--- KeywordDrivenFramework/src/test/java/com/cjc/dsw/webapp/utility/comman.java
+++ KeywordDrivenFramework/src/test/java/com/cjc/dsw/webapp/utility/comman.java
@@ -1,15 +1,38 @@
 package com.cjc.dsw.webapp.utility;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.WebDriver;
 
 public class comman {
 	
-	WebDriver driver;
-	public void getBrowser(WebDriver driver)
+	
+	Properties pro;
+	public static Properties getBrowser() throws IOException
 	{
 		Properties pro=new Properties();
+		
+		FileInputStream fis=new FileInputStream("C:\\Users\\Rasika\\git\\KeyWordDrivernFrameWork\\KeywordDrivenFramework\\src\\test\\resources\\configuration.properties");
+		pro.load(fis);
+		return pro;
+	}
+	
+	public void getExcelVal() throws IOException
+	{
+		File fi=new File("C:\\Users\\Rasika\\git\\KeyWordDrivernFrameWork\\KeywordDrivenFramework\\src\\test\\resources\\ExcelFile\\BillingAdd.xlsx");
+		FileInputStream fis=new FileInputStream(fi);
+		XSSFWorkbook wb=new XSSFWorkbook(fis);
+		XSSFSheet sh=wb.getSheetAt(0);
+		XSSFRow row=sh.getRow(1);
+		XSSFCell us=row.getCell(0);
+		XSSFCell pw=row.getCell(1);
 	}
 
 }
